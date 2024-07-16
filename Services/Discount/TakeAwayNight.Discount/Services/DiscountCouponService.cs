1c525f9c827b98dd5ebc9e83c858123c0f58616c
tuba balkan <tubabalkan165@gmail.com>
2024-07-16 15:28:35 +0300

    ekleme yapıldı

--- Services/Discount/TakeAwayNight.Discount/Services/DiscountCouponService.cs
+++ Services/Discount/TakeAwayNight.Discount/Services/DiscountCouponService.cs
@@ -1,6 +1,32 @@
-﻿namespace TakeAwayNight.Discount.Services
+﻿using TakeAwayNight.Discount.Dtos;
+
+namespace TakeAwayNight.Discount.Services
 {
-    public class DiscountCouponService
+    public class DiscountCouponService : IDiscountCouponService
     {
+        public Task CreateDiscountCouponAsync(CreateDiscountCouponDto createDiscountCouponDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteDiscountCouponAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetByIdDiscountCouponDto> GetByIdDiscountCouponAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ResultDiscountCouponDto>> GetResultDiscountCouponAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateDiscountCouponAsync(UpdateDiscountCouponDto updateDiscountCouponDto)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
