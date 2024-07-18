9960fead573442e0237edece5dac6966860c82e1
tuba balkan <tubabalkan165@gmail.com>
2024-07-18 22:26:09 +0300

    ekleme yapıldı

--- Services/Order/Presentation/TakeAwayNight.OrderApi/Controllers/AddressController.cs
+++ Services/Order/Presentation/TakeAwayNight.OrderApi/Controllers/AddressController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TakeAwayNight.Application.Features.CQRS.Commands.AddressCommands;
 using TakeAwayNight.Application.Features.CQRS.Handlers.AddressHandlers;
+using TakeAwayNight.Application.Features.CQRS.Queries.AddressQueries;
 using TakeAwayNight.Application.Features.CQRS.Results.AddressResults;
 
 namespace TakeAwayNight.OrderApi.Controllers
@@ -9,7 +11,53 @@ namespace TakeAwayNight.OrderApi.Controllers
     [ApiController]
     public class AddressController : ControllerBase
     {
-        private readonly GetAddressQuerHandler _getAddressQuerHandler;
+        private readonly GetAddressQuerHandler _getAddressQueryHandler;
+        private readonly GetAddressByIdQueryHandler _getAddressByIdQueryHandler;
+        private readonly CreateAddressCommandHandler _createAddressCommandHandler;
+        private readonly UpdateAddressCommandHandler _updateAddressCommandHandler;
+        private readonly RemoveAddressCommandHandler _removeAddressCommandHandler;
+
+        public AddressController(GetAddressQuerHandler getAddressQueryHandler, GetAddressByIdQueryHandler getAddressByIdQueryHandler, CreateAddressCommandHandler createAddressCommandHandler, UpdateAddressCommandHandler updateAddressCommandHandler, RemoveAddressCommandHandler removeAddressCommandHandler)
+        {
+            _getAddressQueryHandler = getAddressQueryHandler;
+            _getAddressByIdQueryHandler = getAddressByIdQueryHandler;
+            _createAddressCommandHandler = createAddressCommandHandler;
+            _updateAddressCommandHandler = updateAddressCommandHandler;
+            _removeAddressCommandHandler = removeAddressCommandHandler;
+        }
+        [HttpGet]
+        public async Task<IActionResult> AddressList()
+        {
+            var value = await _getAddressQueryHandler.Handle();
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAddress(CreateAddressCommand command)
+        {
+            await _createAddressCommandHandler.Handle(command);
+            return Ok("Ekleme işlemi yapıldı");
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteAddress(int id)
+        {
+            await _removeAddressCommandHandler.Handle(new RemoveAddressCommand(id));
+            return Ok("Silme işlemi gerçekleşti");
+
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAddress(int id)
+        {
+            var values = await _getAddressByIdQueryHandler.Handle(new GetAddressByIdQuery(id));
+            return Ok(values);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateAddress(UpdateAddressCommand command)
+        {
+            await _updateAddressCommandHandler.Handle(command);
+            return Ok("Güncelleme yapıldı");
+        }
 
     }
 }
